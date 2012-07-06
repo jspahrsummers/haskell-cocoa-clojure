c73d77581689e8b78a0bc77392227025c987554d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-05 20:02:02 -0700

    Whoops, fix character literal parsing to actually match Clojure rules

--- parser.hs
+++ parser.hs
@@ -26,7 +26,6 @@ braces = P.braces lexer
 whiteSpace = skipMany (space <|> char ',')
 
 stringLiteral = liftM StringLiteral $ P.stringLiteral lexer
-characterLiteral = liftM CharacterLiteral $ P.charLiteral lexer
 
 -- The naming here might be confusing for Parsec users, since P.symbol normally
 -- matches any given string, but this matches Clojure terminology
@@ -36,6 +35,26 @@ symbol = liftM (SymbolForm . Symbol) $ P.identifier lexer
 -- TODO: ratio support
 -- TODO: BigDecimal support
 numberLiteral = liftM (either IntegerLiteral DecimalLiteral) $ P.naturalOrFloat lexer
+
+newlineLiteral = do
+    P.symbol lexer "newline"
+    return '\n'
+
+spaceLiteral = do
+    P.symbol lexer "space"
+    return ' '
+
+tabLiteral = do
+    P.symbol lexer "tab"
+    return '\t'
+
+characterLiteral = do
+    char '\\'
+    liftM CharacterLiteral $
+        try newlineLiteral <|>
+        try spaceLiteral <|>
+        try tabLiteral <|>
+        anyChar
 
 nil = do
     reserved "nil"
