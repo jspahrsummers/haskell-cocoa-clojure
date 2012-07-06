a592993922d7c6bc30e5dc50f902574403564a41
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-05 23:32:18 -0700

    Modified build_and_run script to reduce noise and print sensible messages for REPL mode

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
-echo "*** Compiling llvm-clojure"
 ghc -fllvm main.hs
-
-echo "*** Running llvm-clojure on sample input file"
-./main
 
 if [ $? -ne 0 ]
 then
     exit 1
 fi
 
+./main "$@"
+
+if [ $? -ne 0 ]
+then
+    exit 1
+fi
+
+if [ -z "$1" ]
+then
+    # no command-line arguments
+    exit 0
+fi
+
 echo "*** Compiling generated LLVM assembly with llc"
-llc -O0 -o output.S output.ll && clang -fcatch-undefined-behavior -ftrapv -O0 -o output output.S
+llc -O0 -o output.S *.ll && clang -fcatch-undefined-behavior -ftrapv -O0 -o output output.S
 
 if [ $? -ne 0 ]
 then
