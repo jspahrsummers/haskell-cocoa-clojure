2564bd060c5ee2b2589c11fbef9dc41d05bc7c24
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-07 01:17:21 -0700

    Added basic Objective-C syntax types

--- compiler/objccodegen.hs
+++ compiler/objccodegen.hs
@@ -4,13 +4,301 @@ module ObjcCodeGen (codegen, codegenToFile)
 import qualified AST as A
 import Control.Monad.State.Lazy
 import Control.Monad.Writer.Lazy
+import Data.List
 import System.IO
 import Util
 
 codegen :: [A.Form] -> IO String
-codegen forms = return "foo"
+codegen forms = do
+    let st = GeneratorState { counter = 0 }
+    blocks <- evalStateT (codegenMain forms) st
+
+    return $ showDelimList "\n" (sort blocks)
 
 codegenToFile :: Handle -> [A.Form] -> IO ()
 codegenToFile fd forms = do
     str <- codegen forms
     hPutStrLn fd str
+
+codegenMain :: [A.Form] -> GeneratorStateT [BasicBlock]
+codegenMain forms = do
+    (stmts, blocks) <- runWriterT $ genForms forms
+
+    let mainType = FunctionType IntType [IntType, PointerType (PointerType CharType)]
+        mainProto = FuncProto {
+            funcType = mainType,
+            funcName = (Identifier "main"),
+            funcParams = map Identifier ["argc", "argv"]
+        }
+    
+    let retStmt = Return $ IntLiteral 0
+    return $ FuncDef mainProto (stmts ++ [retStmt]) : blocks
+
+genForms :: [A.Form] -> BlockGeneratorT [Statement]
+genForms _ = return []
+
+{-
+    Code generation state
+-}
+
+data GeneratorState = GeneratorState {
+    counter :: Integer
+}
+
+type GeneratorStateT = StateT GeneratorState IO
+
+uniqueId :: GeneratorStateT Integer
+uniqueId = do
+    c <- gets counter
+    put $ GeneratorState {
+        counter = succ c
+    }
+
+    return c
+
+type BlockGeneratorT = WriterT [BasicBlock] GeneratorStateT
+
+{-
+	Objective-C syntax structures
+-}
+
+-- Represents any value for which a C/Objective-C type can be obtained
+class Typeof a where
+    typeof :: a -> Type
+
+-- Any C or Objective-C identifier
+-- (Selectors are not considered identifiers)
+newtype Identifier = Identifier String
+    deriving Eq
+
+instance Show Identifier where
+	show (Identifier s) = s
+
+-- Objective-C selectors
+newtype Selector = Selector String
+    deriving Eq
+
+instance Typeof Selector where
+    typeof s = SelectorType
+
+instance Show Selector where
+    show (Selector s) = "@selector(" ++ s ++ ")"
+
+selectorParts :: Selector -> [String]
+selectorParts (Selector s) =
+    let selectorParts' :: String -> [String] -> [String]
+        selectorParts' s acc =
+            let (part, rest) = break (== ':') s
+            in case rest of
+                [] -> acc ++ [part]
+                ":" -> acc ++ [part ++ ":"]
+                _ -> selectorParts' (tail rest) $ acc ++ [part ++ ":"]
+    in selectorParts' s []
+
+-- Basic language types
+data Type =
+    VoidType |
+    IntType |
+    CharType |
+    BoolType |
+    IdType |
+    ClassType |
+    -- An object instance; technically a pointer, but having a distinct type better represents usage
+    InstanceType Identifier |
+    SelectorType |
+    PointerType Type |
+    -- Used interchangably for both function pointers and function prototypes
+    FunctionType Type [Type] |
+    BlockType Type [Type] |
+    MethodType Type [Type]
+    deriving Eq
+
+instance Typeof Type where
+    typeof t = t
+
+instance Show Type where
+    show VoidType = "void"
+    show IntType = "int"
+    show CharType = "char"
+    show BoolType = "BOOL"
+    show IdType = "id"
+    show ClassType = "Class"
+    show (InstanceType c) = (show c) ++ " *"
+    show SelectorType = "SEL"
+    show (PointerType t) = (show t) ++ " *"
+    show (FunctionType r params) = (show r) ++ " (*)(" ++ (showDelimList ", " params) ++ ")"
+    show (BlockType r params) = (show r) ++ " (^)(" ++ (showDelimList ", " params) ++ ")"
+    -- MethodTypes have no universal code representation
+
+-- Function prototypes
+data FuncProto = FuncProto { funcType :: Type, funcName :: Identifier, funcParams :: [Identifier] }
+    deriving Eq
+
+instance Typeof FuncProto where
+    typeof p = funcType p
+
+instance Show FuncProto where
+    show (FuncProto (FunctionType r pts) id pns) =
+        let showNamedParam :: (Identifier, Type) -> String
+            showNamedParam (p, t) = (show t) ++ " " ++ (show p)
+        in (show r) ++ " " ++ (show id) ++ " (" ++ (intercalate ", " $ map showNamedParam $ zip pns pts) ++ ")"
+
+-- Method prototypes
+data MethodProto = MethodProto { isClassMethod :: Bool, methodType :: Type, methodName :: Selector, methodParams :: [Identifier] }
+    deriving Eq
+
+instance Typeof MethodProto where
+    typeof p = methodType p
+
+instance Show MethodProto where
+    show (MethodProto c (MethodType r pts) sel pns) =
+        let showMethodParts :: [String] -> [Identifier] -> [Type] -> String
+            showMethodParts [selpart] [] [] = selpart
+            showMethodParts selparts pns pts =
+                let showPart :: (String, Identifier, Type) -> String
+                    showPart (s, p, t) = s ++ "(" ++ (show t) ++ ")" ++ (show p)
+                in intercalate " " $ map showPart $ zip3 selparts pns pts
+        in (if c then '+' else '-') : " (" ++ (show r) ++ ")" ++ (showMethodParts (selectorParts sel) pns pts)
+
+-- Basic syntactical unit for file scope
+data BasicBlock =
+    LocalImport FilePath |
+    SystemImport FilePath |
+    FuncDecl FuncProto |
+    FuncDef FuncProto [Statement] |
+    ObjcInterface { className :: Identifier, superclass :: Identifier, protocols :: [Identifier], decls :: [ObjcDecl] } |
+    ObjcProtocol { protocolName :: Identifier, protocols :: [Identifier], decls :: [ObjcDecl] } |
+    -- Can also be used for class extensions (with an empty categoryName)
+    ObjcCategory { className :: Identifier, categoryName :: Identifier, decls :: [ObjcDecl] } |
+    ObjcImplementation { className :: Identifier, categoryName :: Identifier, defs :: [ObjcDef] }
+    deriving Eq
+
+instance Show BasicBlock where
+    show (LocalImport path) = "#import \"" ++ path ++ "\""
+    show (SystemImport path) = "#import <" ++ path ++ ">"
+    show (FuncDecl p) = (show p) ++ ";"
+    show (FuncDef p stmts) = (show p) ++ " {\n" ++ (showDelimList "\n" stmts) ++ "\n}"
+    show (ObjcInterface c sc ps decls) =
+        "@interface " ++ (show c) ++ " : " ++ (show sc) ++ " <" ++ (showDelimList ", " ps) ++ ">\n" ++ (showDelimList "\n" decls) ++ "\n@end"
+
+    show (ObjcProtocol n ps decls) =
+        -- Aggressively forward-declare dependent protocols
+        (concatMap (\p -> "@protocol " ++ (show p) ++ ";\n") ps) ++
+        "@protocol " ++ (show n) ++ " <" ++ (showDelimList ", " ps) ++ ">\n" ++ (showDelimList "\n" decls) ++ "\n@end"
+
+    show (ObjcCategory cl cat decls) =
+        "@interface " ++ (show cl) ++ " (" ++ (show cat) ++ ")\n" ++ (showDelimList "\n" decls) ++ "\n@end"
+
+    show (ObjcImplementation cl (Identifier cat) defs) =
+        "@implementation " ++ (show cl) ++ (if null cat then "\n" else " (" ++ cat ++ ")\n") ++ (showDelimList "\n" defs) ++ "\n@end"
+
+-- BasicBlocks can be sorted by the order they should appear in generated code
+instance Ord BasicBlock where
+    -- Imports
+    compare SystemImport {} _ = LT
+    compare _ SystemImport {} = GT
+    compare LocalImport {} _ = LT
+    compare _ LocalImport {} = GT
+
+    -- Declarations
+    compare FuncDecl {} _ = LT
+    compare _ FuncDecl {} = GT
+    compare ObjcProtocol {} _ = LT
+    compare _ ObjcProtocol {} = GT
+
+    -- Superclasses need to be fully declared before their subclasses
+    -- (Forward declarations will not work)
+    compare ObjcInterface { className = lc, superclass = lsc } ObjcInterface { className = rc, superclass = rsc } =
+        if lc == rsc
+            then LT
+            else if rc == lsc
+                then GT
+                else EQ
+
+    compare ObjcInterface {} ObjcCategory {} = LT
+    compare ObjcCategory {} ObjcInterface {} = GT
+
+    -- Definitions
+    compare FuncDef {} _ = GT
+    compare _ FuncDef {} = LT
+    compare ObjcImplementation {} _ = GT
+    compare _ ObjcImplementation {} = LT
+
+    compare _ _ = EQ
+
+-- Attributes for @property declarations
+data PropertyAttribute =
+    Nonatomic |
+    Readonly |
+    Readwrite |
+    Assign |
+    Strong |
+    Weak |
+    UnsafeUnretained |
+    Copy |
+    Getter Selector
+    deriving Eq
+
+instance Show PropertyAttribute where
+    show Nonatomic = "nonatomic"
+    show Readonly = "readonly"
+    show Readwrite = "readwrite"
+    show Assign = "assign"
+    show Strong = "strong"
+    show Weak = "weak"
+    show UnsafeUnretained = "unsafe_unretained"
+    show Copy = "copy"
+    show (Getter sel) = "getter = " ++ (show sel)
+
+-- Declarations for Objective-C constructs
+data ObjcDecl =
+    PropertyDecl [PropertyAttribute] Type Identifier |
+    MethodDecl MethodProto |
+    ProtocolRequired |
+    ProtocolOptional
+    deriving Eq
+
+instance Typeof ObjcDecl where
+    typeof (PropertyDecl _ t _) = t
+    typeof (MethodDecl p) = typeof p
+
+instance Show ObjcDecl where
+    show (PropertyDecl attrs t id) = "@property (" ++ (showDelimList ", " attrs) ++ ") " ++ (show t) ++ " " ++ (show id) ++ ";"
+    show (MethodDecl proto) = (show proto) ++ ";"
+    show ProtocolRequired = "@required"
+    show ProtocolOptional = "@optional"
+
+-- Definitions for Objective-C constructs
+data ObjcDef =
+    MethodDef MethodProto [Statement]
+    deriving Eq
+
+instance Show ObjcDef where
+    show (MethodDef p stmts) = (show p) ++ " {\n" ++ (showDelimList "\n" stmts) ++ "\n}"
+
+-- Any C or Objective-C expression
+data Expression =
+    VoidExpression |
+    IntLiteral Int
+    deriving Eq
+
+instance Typeof Expression where
+    typeof VoidExpression = VoidType
+    typeof IntLiteral {} = IntType
+
+instance Show Expression where
+    show VoidExpression = "((void)0)"
+    show (IntLiteral i) = show i
+
+-- Statements within a function, method, or block body
+data Statement =
+    EmptyStatement |
+    Statement Expression |
+    Return Expression
+    deriving Eq
+
+instance Show Statement where
+    show EmptyStatement = "\t;"
+    show (Statement expr) = "\t" ++ (show expr) ++ ";"
+    show (Return VoidExpression) = "\treturn;"
+    show (Return expr) = "\treturn " ++ (show expr) ++ ";"
