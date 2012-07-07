270ac7cd136b55a6907a752f3bdf484aa236f046
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-07 03:56:30 -0700

    Added Util.splitOn, for splitting a list into a list of lists

--- compiler/util.hs
+++ compiler/util.hs
@@ -1,7 +1,16 @@
-module Util (showDelimList)
+module Util (showDelimList, splitOn)
     where
 
 import Data.List
 
+-- Shows every value given, delimited by the given string
 showDelimList :: Show a => String -> [a] -> String
 showDelimList s l = intercalate s $ map show l
+
+-- Given a list separated by the given value, returns a list of lists with the separator removed
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn sep l =
+    let (part, rest) = break (== sep) l
+    in if null rest
+        then [part]
+        else [part] ++ (splitOn sep $ tail rest)
