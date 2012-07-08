de0ba027aa7c2035cc54e30a7a45e905b644a27d
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-07 20:13:56 -0700

    Combine REPL module with Main module, since they'll be sharing more and more code

--- compiler/main.hs
+++ compiler/main.hs
@@ -1,12 +1,14 @@
 module Main (main)
     where
 
+import AST
+import Data.Char
 import ObjcCodeGen
 import Parser
-import REPL
 import System.Environment
 import System.Exit
 import System.IO
+import System.Process
 import Text.Parsec
 
 printErrorAndExit :: ParseError -> IO ()
@@ -14,6 +16,14 @@ printErrorAndExit e = do
     putStrLn (show e)
     exitFailure
 
+main :: IO ()
+main = do
+    args <- getArgs
+    if null args then repl else compile args
+
+{-
+    Compilation
+-}
 compile :: [FilePath] -> IO ()
 compile (x:xs) = do
     putStrLn $ "*** Compiling " ++ x
@@ -23,7 +33,65 @@ compile (x:xs) = do
 
     either printErrorAndExit (codegenToFile outFD) (parse Parser.forms x contents)
 
-main :: IO ()
-main = do
-    args <- getArgs
-    if null args then repl else compile args
+{-
+    REPL
+-}
+dropControlCharacters :: String -> (String, String)
+dropControlCharacters "" = ("", "")
+dropControlCharacters s
+    | x == '\EOT' = ("\EOT", "")
+    | x == '\n' = ("\n", dropWhile isSpace $ tail s)
+    | otherwise =
+        let (new_xs, left) = dropControlCharacters $ tail s
+        in (x : new_xs, left)
+    where
+        x = head s
+
+repl' :: String -> [Form] -> IO ()
+repl' s formsSoFar =
+    let (now, left) = dropControlCharacters s
+
+        fixupBackspaces :: String -> Char -> String
+        fixupBackspaces left '\DEL' = init left
+        fixupBackspaces left right = left ++ [right]
+
+    in if last now == '\EOT'
+        then putStrLn "\nQuit"
+        else case (parse Parser.forms "stdin" $ foldl fixupBackspaces "" now) of
+             Left err -> do
+                 putStrLn $ show err
+                 putStr "\n=> "
+                 repl' left formsSoFar
+
+             Right forms -> do
+                 let newForms = formsSoFar ++ forms
+                     objc = codegen newForms
+
+                 putStrLn objc
+
+                 (Just clangIn, _, _, clang) <- createProcess
+                     (proc "clang" ["-L", "lib", "-lCocoaClojureRuntime", "-Iruntime/CocoaClojureRuntime", "-xobjective-c", "-Wno-unused-value", "-framework", "Foundation", "-"])
+                     { std_in = CreatePipe }
+
+                 hPutStrLn clangIn objc
+                 hClose clangIn
+                 clangCode <- waitForProcess clang
+
+                 (_, _, _, aout) <- createProcess $ proc "./a.out" []
+                 progCode <- waitForProcess aout
+
+                 putStr "\n=> "
+
+                 if (clangCode == ExitSuccess) && (progCode == ExitSuccess)
+                    then repl' left newForms
+                    else repl' left formsSoFar
+
+repl :: IO ()
+repl = do
+    hSetBuffering stdin NoBuffering
+    hSetBuffering stdout NoBuffering
+
+    putStr "=> "
+
+    s <- getContents
+    repl' s []
