ee905d76e1ac574fb670beee49a2dda85616cdad
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2012-07-08 16:49:33 -0700

    Factored out delimiting logic from showDelimited into a new delimit function

--- compiler/util.hs
+++ compiler/util.hs
@@ -1,5 +1,9 @@
 module Util (
-        maybeDouble, maybeFile, showDelimited, splitOn
+        delimit,
+        maybeDouble,
+        maybeFile,
+        showDelimited,
+        splitOn
     ) where
 
 import qualified Data.Foldable as Foldable
@@ -18,10 +22,18 @@ maybeDouble r =
     let dbl = fromRational r :: Double
     in if toRational dbl == r then Just dbl else Nothing
 
+-- Joins every string in the given collection with the given delimiter
+delimit :: Foldable.Foldable a => String -> a String -> String
+delimit s c =
+    -- If non-empty...
+    if Foldable.any (\_ -> True) c
+        then Foldable.foldr1 (\l r -> l ++ s ++ r) c
+        else ""
+
 -- Shows every value given, delimited by the given string
 -- The given functor must be non-empty
 showDelimited :: Foldable.Foldable a => Functor a => Show b => String -> a b -> String
-showDelimited s l = Foldable.foldr1 (\l r -> l ++ s ++ r) $ fmap show l
+showDelimited s c = delimit s $ fmap show c
 
 -- Given a list separated by the given value, returns a list of lists with the separator removed
 splitOn :: Eq a => a -> [a] -> [[a]]
